1d238a4ed9edad28b01baa62a5f2773b406c3e92
Kolgolar <62188763+Kolgolar@users.noreply.github.com>
2026-05-08 14:45:55 +0300

    Export

---
+++ src/settings.bb31c.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"0f6e8",main:"bdad4"}};
